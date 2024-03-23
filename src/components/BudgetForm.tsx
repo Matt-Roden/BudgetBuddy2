ae8b6577dd3f714a79da65f6a3a78e1fe13f00e6
Matt Roden <matt.roden@tackle.io>
2024-03-23 14:34:03 -0600

    Complete basic create new budget form funcitonality - Not styled

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,31 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 type Expense = {
   amount: string;
   type: string;
+  id: string;
+};
+
+type Budget = {
+  name: string;
+  totalSavings: number;
+  income: number;
+  expenses: Expense[];
 };
 
 const BudgetForm = () => {
+  const [budgetName, setBudgetName] = useState<string>("");
   const [income, setIncome] = useState<number>(0);
   const [expenses, setExpenses] = useState<Expense[]>([
-    { amount: "", type: "" },
+    { amount: "", type: "", id: uuidv4() },
   ]);
+  const [totalSavings, setTotalSavings] = useState<number>(0);
 
-  const handleAddExpense = (e: React.FormEvent) => {
-    e.preventDefault();
-    setExpenses([...expenses, { amount: "", type: "" }]);
+  const findTotalSavings = () => {
+    const expensesTotal = expenses.reduce((accum, currVal) => {
+      accum += Number(currVal.amount);
+      return accum;
+    }, 0);
+
+    return setTotalSavings(income - expensesTotal);
   };
+
+  const handleAddExpense = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setExpenses([...expenses, { amount: "", type: "", id: uuidv4() }]);
+  };
+
+  const removeExpense = (expenseId: string) => {
+    const filteredExpenses = expenses.filter(
+      (expense) => expense.id !== expenseId
+    );
+    setExpenses(filteredExpenses);
+  };
+
+  const handleExpenseAmountChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    expense: Expense
+  ) => {
+    const updatedExpenses: React.SetStateAction<Expense[]> = [];
+    expenses.forEach((item) => {
+      if (item.id === expense.id) {
+        item.amount = e.target.value;
+        updatedExpenses.push(item);
+      } else {
+        updatedExpenses.push(item);
+      }
+    });
+    setExpenses(updatedExpenses);
+  };
+  const handleExpenseTypeChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    expense: Expense
+  ) => {
+    const updatedExpenses: React.SetStateAction<Expense[]> = [];
+    expenses.forEach((item) => {
+      if (item.id === expense.id) {
+        item.type = e.target.value;
+        updatedExpenses.push(item);
+      } else {
+        updatedExpenses.push(item);
+      }
+    });
+    setExpenses(updatedExpenses);
+  };
+
+  const handleSaveBudget = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const budget: Budget = {
+      name: budgetName,
+      income,
+      expenses: expenses.filter((item) => item.amount),
+      totalSavings,
+    };
+
+    console.log(budget);
+  };
+
+  useEffect(() => {
+    findTotalSavings();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [expenses, income]);
 
   return (
     <div>
-      <h3>BudgetForm</h3>
-      <form>
+      <h3>Create New Budget</h3>
+      <form onSubmit={(e) => handleSaveBudget(e)}>
         <div>
           <label htmlFor="budget-name">Budget Name *</label>
-          <input id="budget-name" />
+          <input
+            id="budget-name"
+            value={budgetName}
+            onChange={(e) => setBudgetName(e.target.value)}
+          />
         </div>
         <div>
-          <label htmlFor="income-amount-field'">Income</label>
+          <label htmlFor="income-amount-field'">Income *</label>
           <input
             id="income-amount-field"
             value={income}
@@ -35,24 +114,58 @@ const BudgetForm = () => {
           />
         </div>
         <div>
-          {expenses.map((expense, index) => {
+          <button
+            onClick={(e) => {
+              handleAddExpense(e);
+            }}
+          >
+            Add Expense
+          </button>
+        </div>
+        <div>
+          {expenses.map((expense) => {
             return (
-              // Todo: figure out how to handle this input's onChange
-              // Formik? find out how to do without formik though!
-              <div key={index}>
+              <div key={expense.id}>
                 <label htmlFor="expense-type">Expense Type</label>
-                <input id="expense-type" value={expense.type} />
+                <input
+                  id="expense-type"
+                  value={expense.type}
+                  onChange={(e) => handleExpenseTypeChange(e, expense)}
+                />
                 <label htmlFor="expense-amount">Amount</label>
-                <input id="expense-amount" value={expense.amount} />
+                <input
+                  id="expense-amount"
+                  type="number"
+                  value={expense.amount}
+                  onChange={(e) => {
+                    handleExpenseAmountChange(e, expense);
+                  }}
+                />
+                <button
+                  disabled={expenses.length <= 1}
+                  onClick={() => {
+                    removeExpense(expense.id);
+                  }}
+                >
+                  Remove
+                </button>
               </div>
             );
           })}
         </div>
         <div>
-          <button onClick={(e) => handleAddExpense(e)}>Add Expense</button>
-        </div>
-        <div>
-          <button>Save Budget</button>
+          <div>Total Savings: ${totalSavings}</div>
+          {/* Todo: on save will POST this budget to an API*/}
+          <button
+            type="submit"
+            disabled={
+              expenses.filter((item) => item.amount).length < 1 ||
+              !income ||
+              !budgetName
+            }
+          >
+            Save Budget
+          </button>
         </div>
       </form>
     </div>
